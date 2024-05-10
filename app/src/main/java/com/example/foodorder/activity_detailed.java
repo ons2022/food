77a2a6832f84ca9a648d36346fb7d4f0a7141ab4
ons2022 <onsismailhajri@gmail.com>
2024-05-10 13:08:11 +0100

    first commit

---
+++ app/src/main/java/com/example/foodorder/activity_detailed.java
@@ -0,0 +1,36 @@
+package com.example.foodorder;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.foodorder.databinding.ActivityDetailedBinding;
+
+public class activity_detailed extends AppCompatActivity {
+
+    ActivityDetailedBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDetailedBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Intent intent = this.getIntent();
+        if (intent != null){
+            String name = intent.getStringExtra("name");
+            String time = intent.getStringExtra("time");
+            int ingredients = intent.getIntExtra("ingredients", R.string.maggiIngredients);
+            int desc = intent.getIntExtra("desc", R.string.maggieDesc);
+            int image = intent.getIntExtra("image", R.drawable.img);
+
+            binding.detailName.setText(name);
+            binding.detailTime.setText(time);
+            binding.detailDesc.setText(desc);
+            binding.detailIngredients.setText(ingredients);
+            binding.detailImage.setImageResource(image);
+        }
+    }
+}
