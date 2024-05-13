b56088ca8c63e0910a8ac8dc1f750f7b79e8c3fd
ons2022 <onsismailhajri@gmail.com>
2024-05-14 00:25:24 +0100

    ons

--- app/src/main/java/com/example/foodorder/activity_detailed.java
+++ app/src/main/java/com/example/foodorder/activity_detailed.java
@@ -3,6 +3,8 @@ package com.example.foodorder;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,6 +20,7 @@ public class activity_detailed extends AppCompatActivity {
         binding = ActivityDetailedBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+
         Intent intent = this.getIntent();
         if (intent != null){
             String name = intent.getStringExtra("name");
@@ -32,5 +35,17 @@ public class activity_detailed extends AppCompatActivity {
             binding.detailIngredients.setText(ingredients);
             binding.detailImage.setImageResource(image);
         }
+
+        Button addButton = findViewById(R.id.button_add_recipe);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start a new activity when the button is clicked
+                Intent intent = new Intent(activity_detailed.this, activity_upload.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
